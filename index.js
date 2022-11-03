dd80cec4d25c8747beb4e23eaad59f0b3313e43c
Mugesh Kumar K <105528383+Mugesh2297@users.noreply.github.com>
2022-11-03 10:11:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post("/forgotpassword", cors(), async (req, res, next) => {
       to: email,
       subject: "Password Reset",
       text: "Click this Link to Reset Your Password",
-      html: `<a href=${link} target="_blank">Click Here to Reset Your Password</a>`
+      html: `<a href=${link} target="_blank"><button>Click Here to Reset Your Password</button></a>`
     };
     transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
